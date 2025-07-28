076ca4ba161d13a3160c70283ad884bc1f3693f4
Vardhan <vrdhn0@gmail.com>
2025-07-28 12:14:04 +0530

    Fixes #13: basic /groups being live

---
+++ test/gap_web/live/group_test.exs
@@ -0,0 +1,10 @@
+defmodule GapWeb.Live.GroupLiveTest do
+  use GapWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  test "displays the welcome message", %{conn: conn} do
+    {:ok, view, html} = live(conn, ~p"/groups")
+
+    assert html =~ "Group Membership"
+  end
+end
