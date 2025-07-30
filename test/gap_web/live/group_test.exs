c878a7463d0b3639ee140cc9f5509ba9c7912c42
Vardhan <vrdhn0@gmail.com>
2025-07-30 14:03:06 +0530

    Fixes #19: Adding tests for new-group button

--- test/gap_web/live/group_test.exs
+++ test/gap_web/live/group_test.exs
@@ -7,4 +7,22 @@ defmodule GapWeb.Live.GroupLiveTest do
 
     assert html =~ "Your groups"
   end
+
+  test "add group", %{conn: conn} do
+    {:ok, view, html_initial} = live(conn, ~p"/groups")
+
+    {:ok, document} = Floki.parse_document(html_initial)
+
+    assert Floki.find(document, "table#groups-list tbody tr") |> length() == 0
+
+    # Click the button
+    html_grp1 =
+      view
+      |> element("#new_group_button")
+      |> render_click()
+
+    {:ok, document} = Floki.parse_document(html_grp1)
+    IO.inspect(document)
+    assert Floki.find(document, "table#groups-list tbody tr") |> length() == 1
+  end
 end
