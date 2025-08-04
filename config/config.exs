f15cbdd40dca8704413b7a19ef31f1888f2d8150
Vardhan <vrdhn0@gmail.com>
2025-08-04 09:45:19 +0530

    Fixes #29: Adding credo, dialyxir, mix_test_watch

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,14 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+if Mix.env() == :dev do
+  config :mix_test_watch,
+    tasks: [
+      "format",
+      "test"
+    ]
+end
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- precommit.sh
+++ precommit.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 
+mix format
 mix clean
 mix coveralls | sed -n '/^[-]\{3,\}$/,/^[-]\{3,\}$/p' > coverage.txt
 mix coveralls
 git diff coverage.txt 
+mix credo --strict
+
+# mix dialyzer
